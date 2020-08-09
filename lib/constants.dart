dc2e5dfd0ccde6de826fa9ff702030a07c92a5a6
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2020-08-10 03:41:57 +0530

    Added dependencies
    -> path
    -> sqflite
    -> uuid
    
    Added global variable 'db' to constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
+import 'package:google_calendar_wrapper/imports.dart';
+
 const String colorApi =
     'https://www.googleapis.com/calendar/v3/colors?key=AIzaSyBsXOW1dJTMJH0-VwVNGeecUf_F5M4Ht08';
 Map<String, dynamic> COLORS;
+Database db;
--- lib/imports.dart
+++ lib/imports.dart
@@ -3,9 +3,13 @@ export 'package:flutter/material.dart';
 export 'dart:convert';
 export 'dart:math';
 
-export 'package:intl/intl.dart' hide TextDirection;
 export 'package:google_sign_in/google_sign_in.dart';
 export 'package:googleapis/calendar/v3.dart' hide Colors;
+
+export 'package:intl/intl.dart' hide TextDirection;
+export 'package:path/path.dart';
+export 'package:sqflite/sqflite.dart';
+export 'package:uuid/uuid.dart';
 
 export 'package:google_calendar_wrapper/models/_models.dart';
 export 'package:google_calendar_wrapper/pages/_pages.dart';
